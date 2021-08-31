9b37521f04b028621f5aaca5619b9962520c69c2
ksenia nesytova <alleheldentot@gmail.com>
2021-08-31 12:56:24 +0700

    added mock data
    app now returns separate parent comments

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
 import './App.css';
+import Comment from './Components/Comment'
+
+
+const commentData = {
+  title: "Fake article title.",
+  author: "grzm",
+  comments: [
+    {
+      id: 1,
+      text: "Example comment here.",
+      author: "user2",
+      children: [
+        {
+          id: 2,
+          text: "Another example comment text.",
+          author: "user3",
+          children: [
+            {
+              id: 3,
+              text: "Another example comment text.",
+              author: "user4",
+              children: []
+            }
+          ]
+        }
+      ]
+    },
+    {
+      id: 4,
+      text: "Example comment here 2.",
+      author: "user5",
+      children: []
+    }
+  ]
+}
+
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <div>
+        {
+          commentData.comments.map((comment) => {
+            return (
+              <Comment key={comment.id} comment={comment} />
+            )
+          })
+        }
+      </div>
   );
 }
 
